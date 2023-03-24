2c0d310f09e0037dad96fd999812a7967d433253
Zookerman3 <szook7@gmail.com>
2023-03-24 12:17:06 -0700

    added full CRUD for Treats

---
+++ Pierres/Controllers/TreatsController.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Pierres.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Pierres.Controllers
+{
+    [Authorize]
+    public class TreatsController : Controller
+    {
+        private readonly PierresContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TreatsController(UserManager<ApplicationUser> userManager, PierresContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            List<Treat> userTreats = _db.Treats
+                                .Where(entry => entry.User.Id == currentUser.Id)
+                                .ToList();
+            return View(userTreats);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Treat treat)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+                treat.User = currentUser;
+                _db.Treats.Add(treat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            Treat thisTreat = _db.Treats
+                .Include(treat => treat.JoinEntities)
+                .ThenInclude(join => join.Flavor)
+                .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat)
+        {
+            _db.Treats.Update(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+           Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
